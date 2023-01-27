d79f5c343aa82ccb1f8543124137fc08e20d5085
Lucas Matheus <62152683+lucasgomes14@users.noreply.github.com>
2023-01-27 14:58:04 -0300

    Primeiro exercício de SET
    Primeiro Exercício sobre o conjunto SET

---
+++ SET/exercicioResolvido/src/entities/LogEntry.java
@@ -0,0 +1,56 @@
+package entities;
+
+import java.util.Date;
+
+public class LogEntry {
+	private String username;
+	private Date moment;
+
+	public LogEntry(String username, Date moment) {
+		this.username = username;
+		this.moment = moment;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public Date getMoment() {
+		return moment;
+	}
+
+	public void setMoment(Date moment) {
+		this.moment = moment;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((username == null) ? 0 : username.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		LogEntry other = (LogEntry) obj;
+		if (username == null) {
+			if (other.username != null)
+				return false;
+		} else if (!username.equals(other.username))
+			return false;
+		return true;
+	}
+
+	
+}
