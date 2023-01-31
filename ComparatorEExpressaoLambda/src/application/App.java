62c0f955ae9a7c858c008dda2096493d3b0848f1
Lucas Matheus <62152683+lucasgomes14@users.noreply.github.com>
2023-01-31 15:41:53 -0300

    Começo do capítulo de expressão lambda
    Comparator com expressão lambda

---
+++ ComparatorEExpressaoLambda/src/application/App.java
@@ -0,0 +1,23 @@
+package application;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import entities.Product;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+		
+        List<Product> list = new ArrayList<>();
+
+		list.add(new Product("TV", 900.00));
+		list.add(new Product("Notebook", 1200.00));
+		list.add(new Product("Tablet", 450.00));
+
+		list.sort( (p1, p2) -> p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase()));
+
+		for(Product p : list) {
+			System.out.println(p);
+		}
+    }
+}
