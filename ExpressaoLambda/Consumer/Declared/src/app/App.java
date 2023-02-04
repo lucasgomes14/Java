28de93a36434af0706805c5852c35f60a63f6671
Lucas Matheus <62152683+lucasgomes14@users.noreply.github.com>
2023-02-03 23:11:48 -0300

    Consumer labda declared

---
+++ ExpressaoLambda/Consumer/Declared/src/app/App.java
@@ -0,0 +1,31 @@
+package app;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.function.Consumer;
+
+import entities.Product;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Locale.setDefault(Locale.US);
+
+		List<Product> list = new ArrayList<>();
+
+		list.add(new Product("Tv", 900.00));
+		list.add(new Product("Mouse", 50.00));
+		list.add(new Product("Tablet", 350.50));
+		list.add(new Product("HD Case", 80.90));
+
+		double factor = 1.1;
+
+		Consumer<Product> cons = p -> {
+			p.setPrice(p.getPrice() * factor);
+		};
+
+		list.forEach(cons);
+
+		list.forEach(System.out::println);
+    }
+}
