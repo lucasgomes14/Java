83f6b8836e8f4a4ca9808280553dd84928c9b08f
Lucas Matheus <62152683+lucasgomes14@users.noreply.github.com>
2023-02-03 23:42:40 -0300

    Function non static

---
+++ ExpressaoLambda/Function/Non static/src/app/App.java
@@ -0,0 +1,27 @@
+package app;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+import entities.Product;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Locale.setDefault(Locale.US);
+
+		List<Product> list = new ArrayList<>();
+
+		list.add(new Product("Tv", 900.00));
+		list.add(new Product("Mouse", 50.00));
+		list.add(new Product("Tablet", 350.50));
+		list.add(new Product("HD Case", 80.90));
+
+		List<String> names = list.stream().map(Product::nonStaticUpperCaseNames).collect(Collectors.toList());
+
+		names.forEach(System.out::println);
+
+    }
+}
